e3f810db650b435f5a1ccce09de9214af03cfe3b
Bruno Novais <git@brnova.is>
2023-12-15 08:25:47 -0300

    chore: initial prettier configuration

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
