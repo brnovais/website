faef1baffe9fefd0b90159ce989f0606da2ed5ff
Bruno Novais <git@brnova.is>
2023-12-15 08:38:35 -0300

    chore: adding tailwind plugin to prettier

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -6,7 +6,7 @@ export default {
   semi: true,
   singleQuote: true,
   trailingComma: 'es5',
-  plugins: ['prettier-plugin-astro'],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   overrides: [
     {
       files: '*.astro',
