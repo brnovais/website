4b400202581d7c6032c9ed2097debfc3a430a837
Bruno Novais <git@brnova.is>
2023-12-26 19:05:34 -0300

    feat: initial article used for testing

---
+++ src/content/articles/primeiro-artigo.md
@@ -0,0 +1,11 @@
+---
+title: 'Primeiro artigo'
+description: 'Primeiro artigo'
+author: 'Bruno Novais'
+date: 2023-12-26
+tags: ['astro', 'github']
+---
+
+### Primeiro artigo
+
+Primeiro artigo.
---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { z, defineCollection } from 'astro:content';
+
+const articlesCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    author: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()),
+    draft: z.boolean().optional(),
+  }),
+});
+
+export const collections = {
+  articles: articlesCollection,
+};
